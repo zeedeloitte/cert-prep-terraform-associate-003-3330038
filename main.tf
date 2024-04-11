fa05a685902a80d43865e0060ca9ab1058238fd0
Mohammad Zeeshan <YU43SB@insim.biz>
2024-04-11 08:51:19 +0200

    update

--- main.tf
+++ main.tf
@@ -1,19 +1,19 @@
- resource "aws_instance" "ec2-instance1" {
-   ami = "ami-0005e0cfe09cc9050"
-   instance_type = "t2.micro"
+resource "aws_instance" "ec2-instance1" {
+  ami           = "ami-0035ee596a0a12a7b"
+  instance_type = "t2.micro"
 
-   tags = {                                
-     Name = "demo-server1"
-}
+  tags = {
+    Name = "demo-server1"
+  }
 }
 
 
 resource "aws_instance" "ec2-Instance2" {
   provider      = aws.west
-  ami           = "ami-0a5ed7a812aeb495a"  # AMI for us-west-1
+  ami           = "ami-0b990d3cfca306617" # AMI for us-west-1
   instance_type = "t2.micro"
 
-  tags = {                                
+  tags = {
     Name = "demo-server2"
-}
+  }
 }
